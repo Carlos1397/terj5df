946db34c42a3a9c1a2a71af56c55c3ca64e68919
Carlos Viera <pisis_13_3@hotmail.com>
2022-01-23 22:20:57 -0500

    Agregando datos a tabla desde formulario

--- src/App.js
+++ src/App.js
@@ -1,20 +1,56 @@
 import React, { Component } from 'react';
 
 class App extends Component {
+  constructor() {
+    super();
+    this.state = {
+      data: [],
+      nombre: "",
+      apellido: ""
+    };
+    this.inputNombre = this.inputNombre.bind(this);
+    this.inputApellido = this.inputApellido.bind(this);
+    this.agregaData = this.agregaData.bind(this);
+  }
+  inputNombre(e) {
+    this.setState({
+      nombre: e.target.value
+    });
+  }
+  inputApellido(e) {
+    this.setState({
+      apellido: e.target.value
+    });
+  }
+  agregaData(e) {
+    e.preventDefault();
+    let data = this.state.data;
+    let objdata = {
+      id: this.state.data.length + 1,
+      nombre: this.state.nombre,
+      apellido: this.state.apellido
+    }
+    this.setState({
+      data: [...data, objdata],
+      nombre: "",
+      apellido: ""
+    });
+  }
   render() {
+    const { nombre, apellido, data } = this.state;
     return (
       <div className="container">
         <div className="row">
           <div className="col-sm-6 col-sm-offset-3">
-            <form>
+            <form onSubmit={this.agregaData}>
               <div className="form-group">
                 <label htmlFor="first-name">Nombre</label>
-                <input type="text" className="form-control" name="first-name" />
+                <input value={nombre} onChange={this.inputNombre} type="text" className="form-control" name="first-name" />
               </div>
 
               <div className="form-group">
-                <label htmlFor="last-name">Apellido</label>
-                <input type="text" className="form-control" name="last-name" />
+                <label htmlFor="last-name">Apellido </label>
+                <input value={apellido} type="text" onChange={this.inputApellido} className="form-control" name="last-name" />
               </div>
 
               <div className="action">
@@ -30,7 +66,12 @@ class App extends Component {
                 </tr>
               </thead>
               <tbody>
-
+                {data.map((element) =>
+                  <tr key={element.id}>
+                    <td>{element.nombre}</td>
+                    <td>{element.apellido}</td>
+                  </tr>
+                )}
               </tbody>
             </table>
           </div>
